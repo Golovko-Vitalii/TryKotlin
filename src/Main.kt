9124ca3e2e2d72466dfaa9946200a47d45157f67
Vitalii Golovko <golovko.inc@gmail.com>
2018-08-30 15:58:08 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    var name1: String
+    name1 = "VG"
+    var name2: String = "VG"
+    var a: Int = 1
+    var b: Int = 2
+    println("Hello " + name1 + ". Try Kotlin!")
+    println("Hello \$name2 $name2! It's my first program in Kotlin.")
+    println("A + B = ${a + b}")
+}
